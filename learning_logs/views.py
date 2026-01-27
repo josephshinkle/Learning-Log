35d2de3fe96751507f2a606f1135896b8a6591f4
Joseph Shinkle <josephshinkle11@icloud.com>
2026-01-26 21:32:34 -0600

    Refactored code into check_topic_owner function and restricted new entry to outside users

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -21,8 +21,7 @@ def topic(request, topic_id):
     """Show a single topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
     # Make sure the topic belongs to the current user
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic)
     
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
@@ -51,6 +50,7 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         # No data submitted; create a blank form
@@ -73,8 +73,7 @@ def edit_entry(request, entry_id):
     """Edit an exisiting entry"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         # Initial request; pre-fill form with current entry
@@ -88,3 +87,8 @@ def edit_entry(request, entry_id):
         
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+def check_topic_owner(request, topic):
+    """Make sure the topic is associated with the right user"""
+    if topic.owner != request.user:
+        raise Http404
